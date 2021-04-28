7c36590a2cfe812ba6ae9d17d9698458b6913afc
Giovani <giovanigonzalezchavez@gmail.com>
2021-04-28 18:20:06 -0500

    Modificado

---
+++ src/AddUserForm.jsx
@@ -0,0 +1,46 @@
+import React, { Fragment } from 'react';
+import { useForm } from 'react-hook-form';
+
+const AddUserForm = (props) => {
+
+  const { register, errors, handleSubmit } = useForm();
+
+  const onSubmit = (data, e) => {
+    console.log(data);
+    props.addUser(data);
+    //limpiar los campos
+    e.target.reset();
+  }
+
+
+  return (
+    <Fragment>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        ref={
+          register({
+            required: { value:true, message:'Campo requerido'}
+          })
+        } />
+      <div>{errors?.name?.message}</div>
+      <label>Username</label>
+      <input
+        type="text"
+        name="username"
+        ref={
+          register({
+            required: { value:true, message:'Campo requerido'}
+          })
+        } />
+      <div>{errors?.username?.message}</div>
+      <button>Add new user</button>
+      </form>
+    </Fragment>
+    
+  )
+}
+
+export default AddUserForm
